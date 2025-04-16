7b7963129ddea4372817ed633eaf98f35de254e5
Thalison Aragão <thalissonaragao01@gmail.com>
2025-04-16 15:49:28 -0300

    Extensão de interfaces

--- Ignite-feed/src/components/Comment.tsx
+++ Ignite-feed/src/components/Comment.tsx
@@ -22,7 +22,7 @@ export function Comment({content, onDeleteComment}: CommentProps){
 
     return(
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/thalisonaragao.png" alt=''/>
+            <Avatar hasBorder={false} src="https://github.com/thalisonaragao.png" />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
